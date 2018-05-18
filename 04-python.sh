e19e568800e83470bd37d75dd53eb9d342f377b7
Matt Mollison <matt.mollison@gmail.com>
2018-05-18 11:20:59 -0700

    no env, no prompts, fewer packages

--- 04-python.sh
+++ 04-python.sh
@@ -36,9 +36,9 @@ export PATH="$MC_DIR/bin:$PATH"
 # conda config --set always_yes yes
 
 # -q for quiet
-conda update -q conda
+conda update -q conda -y
 
-conda install -n root _license
+conda install -n root _license -y
 
 conda upgrade -y --all
 
@@ -66,53 +66,55 @@ jupyter_contrib_nbextensions
 s3fs
 networkx'
 
-# # Install packages in root environment
-# conda install $packages -y
+# Install packages in root environment
+conda install $packages -y
 
-# Create separate environment called py3
-conda create -q --name py3 python=3 $packages -y
-source activate py3
+# # Create separate environment called py3
+# conda create -q --name py3 python=3 $packages -y
+# source activate py3
 
-# Dataset profiling and loading
+# Dataset loading and profiling
 pip install -U xlrd
 pip install -U pandas-profiling # https://github.com/pandas-profiling/pandas-profiling
 pip install -U missingno # https://github.com/ResidentMario/missingno
-pip install -U imbalanced-learn
-pip install -U pandas-datareader # https://github.com/pydata/pandas-datareader
-# pip install -U kaggle-cli
+
+# # External datasets
+# pip install -U pandas-datareader # https://github.com/pydata/pandas-datareader
+# # pip install -U kaggle-cli
 
 # Utility packages
-pip install -U graphviz
 pip install -U tqdm
 pip install -U pytest
+pip install -U graphviz
 
-# ML packages
-pip install -U lightgbm # https://github.com/Microsoft/LightGBM
-pip install -U xgboost
+# # ML packages
+# pip install -U imbalanced-learn
+# pip install -U lightgbm # https://github.com/Microsoft/LightGBM
+# pip install -U xgboost
 
-# Foundational NLP packages
-pip install -U spacy # https://spacy.io/
-python -m spacy download en_core_web_lg
-pip install -U gensim # https://radimrehurek.com/gensim/
-pip install -U nltk
+# # Foundational NLP packages
+# pip install -U spacy # https://spacy.io/
+# python -m spacy download en_core_web_lg
+# pip install -U gensim # https://radimrehurek.com/gensim/
+# pip install -U nltk
 
-# NLP building on top of spaCy or others
-pip install -U textacy # https://github.com/chartbeat-labs/textacy
-# pip install -U thinc # https://github.com/explosion/thinc
-# pip install -U pattern # https://github.com/clips/pattern
+# # NLP building on top of spaCy or others
+# pip install -U textacy # https://github.com/chartbeat-labs/textacy
+# # pip install -U thinc # https://github.com/explosion/thinc
+# # pip install -U pattern # https://github.com/clips/pattern
 
-# NLP utilities
-pip install -U ftfy # https://github.com/LuminosoInsight/python-ftfy
-pip install -U fuzzywuzzy # https://github.com/seatgeek/fuzzywuzzy
-pip install -U python-Levenshtein # for fuzzywuzzy
-pip install -U pyldavis # https://github.com/bmabey/pyLDAvis
+# # NLP utilities
+# pip install -U ftfy # https://github.com/LuminosoInsight/python-ftfy
+# pip install -U fuzzywuzzy # https://github.com/seatgeek/fuzzywuzzy
+# pip install -U python-Levenshtein # for fuzzywuzzy
+# pip install -U pyldavis # https://github.com/bmabey/pyLDAvis
 
 # # Neural network packages
 # pip install -U tensorflow
 # pip install -U keras
 # # http://pytorch.org/
 
-# Add Jupyter kernel for this environment and set the display name
-# http://ipython.readthedocs.io/en/stable/install/kernel_install.html#kernels-for-different-environments
-python -m ipykernel install --user --name py3 --display-name "py3"
-source deactivate
+# # Add Jupyter kernel for this environment and set the display name
+# # http://ipython.readthedocs.io/en/stable/install/kernel_install.html#kernels-for-different-environments
+# python -m ipykernel install --user --name py3 --display-name "py3"
+# source deactivate
