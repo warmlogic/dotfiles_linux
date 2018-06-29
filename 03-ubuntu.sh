5089ff04ad84557923502ab4d6affb376fd8a888
Matt Mollison <matt.mollison@gmail.com>
2018-06-29 12:35:25 -0700

    install fonts

--- 03-ubuntu.sh
+++ 03-ubuntu.sh
@@ -8,6 +8,18 @@ sudo apt-get update
 sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install build-essential gcc g++ make binutils htop tmux emacs software-properties-common swig
 
+# Fonts for matplotlib
+sudo apt-get --assume-yes install font-manager
+
+# # Optional: MS fonts
+# echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+# sudo apt-get --assume-yes install ttf-mscorefonts-installer
+# # remove with: sudo apt-get remove --purge ttf-mscorefonts-installer
+
 # For editing in SublimeText on a remote computer
+# Requires additional setup: http://blog.macromates.com/2011/mate-and-rmate/
+# 1. Add to ~/.ssh/config entry: RemoteForward 52698 localhost:52698
+# 2. SSH into server
+# 3. Run to edit in SublimeText: rsub myfile.txt
 sudo wget -O /usr/local/bin/rsub https://raw.github.com/aurora/rmate/master/rmate
 sudo chmod +x /usr/local/bin/rsub
